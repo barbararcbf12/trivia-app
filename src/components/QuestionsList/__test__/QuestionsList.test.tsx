42b0936cfa046754632ac391d39cc19d6303d210
Barbara Ferreira <bbf@charlietango.dk>
2025-01-04 19:06:20 +0100

    feat: add test to the QuestionsList component

---
+++ src/components/QuestionsList/__test__/QuestionsList.test.tsx
@@ -0,0 +1,7 @@
+import QuestionsList from "../QuestionsList";
+import { render } from "@testing-library/react";
+import { questions } from "../../../api/mockData";
+
+test("should render <QuestionsList> without props", () => {
+  render(<QuestionsList questions={ questions } />);
+});
