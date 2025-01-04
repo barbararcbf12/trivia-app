0dcec7f709e29c321dfd408568f275fcdb4289fe
Barbara Ferreira <bbf@charlietango.dk>
2025-01-04 23:55:46 +0100

    refactor: adjust bg color of form

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import Header from "./components/Header/Header";
 import QuestionsList from "./components/QuestionsList/QuestionsList";
 import { ReactQueryProvider } from "./api/react-query-provider";
+import { QuestionsContextProvider } from "./context/QuestionsContext";
 
 function App() {
   return (
-    <ReactQueryProvider>
-      <div className="space-y-3 flex flex-col items-center">
-        <Header />
-        <main className="p-3 w-1/2 flex justify-center">
-          <QuestionsList />
-        </main>
-      </div>
-    </ReactQueryProvider>
+    <div className="w-full h-full flex flex-col items-center">
+      <ReactQueryProvider>
+        <QuestionsContextProvider>
+          <Header />
+          <main className="w-full h-full flex flex-grow justify-center p-3 bg-mono-100">
+            <QuestionsList />
+          </main>
+        </QuestionsContextProvider>
+      </ReactQueryProvider>
+    </div>
   );
 }
 
