0e9ca1d5c051007abf3c71ad6026a16137e9995e
Barbara Ferreira <bbf@charlietango.dk>
2025-01-04 23:58:03 +0100

    refactor: use context in QuestionsList

--- src/components/QuestionsList/QuestionsList.tsx
+++ src/components/QuestionsList/QuestionsList.tsx
@@ -1,53 +1,50 @@
-import React, { useState } from "react";
+import React, { FormEvent } from "react";
 import Question from "../Question/Question";
-import { useQuery } from "@tanstack/react-query";
-import { getQuestions } from "../../api/getQuestions";
-import type { ApiDataProps } from "../../types/trivia-api";
-import type { AnswerProps } from "../../types/answer";
+import Button from "../Button/Button";
+import { useQuestions } from "../../context/QuestionsContext";
 
 function QuestionsList() {
-  const [ isSubmitted, setIsSubmitted ] = useState<boolean>(false);
-  const [ answers, setAnswers ] = useState<AnswerProps[]>([]);
+  const {
+    questions,
+    isLoading,
+    isFetching,
+    error,
+    isSuccess,
+    setEnabled,
+    isFormSubmitted,
+    setIsFormSubmitted,
+    answers,
+  } = useQuestions();
 
-  const { isLoading, error, data, isSuccess } = useQuery<ApiDataProps>({
-    queryKey: ['triviaData'],
-    queryFn: getQuestions,
-  });
-
-  function onHandleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  function onHandleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    setIsSubmitted(true);
+    setIsFormSubmitted(true);
+    setEnabled(false);
   }
 
   const countCorrectAnswers = answers.filter(answer => answer.isCorrect).length;
+  const isFormFilled = answers.length === questions.length;
 
   return (
     <section className="flex flex-col gap-4 items-center w-1/2">
-      { isLoading ? <span className="loader"></span> : null }
+      { isLoading || isFetching ? <span className="loader"></span> : null }
       { error ? <span>{ `An error has occurred: ${ error.message }` }</span> : null }
-      { isSuccess ?
+      { !isLoading && !error && !isFetching && isSuccess ?
         <>
           <form
             onSubmit={ onHandleSubmit }
             className="w-full flex flex-col space-y-4"
           >
-            { data?.results?.map((question, index) =>
+            { questions?.map((question, index) =>
               <Question
                 key={ question.question }
                 questionId={ `question-${ index + 1 }` }
-                answers={ answers }
-                handleSelect={ setAnswers }
-                isFormSubmitted={ isSubmitted }
                 { ...question }
               />
             ) }
-            <button
-              className="w-fit bg-grey-300 py-2 px-4 rounded-minimal text-white"
-            >
-              Submit
-            </button>
+            <Button disabled={ !isFormFilled }>Submit</Button>
           </form>
-          { isSubmitted ? <span className="text-red-600">{ `${countCorrectAnswers} / 10 correct` }</span> : null }
+          <span className="text-red-600 pb-6 mb-6">{ isFormSubmitted ? `${ countCorrectAnswers } / 10 correct` : ''}</span>
         </>
       : null }
     </section>
