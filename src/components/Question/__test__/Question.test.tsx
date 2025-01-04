355741ac576d913f3bc985d705876e8378936d93
Barbara Ferreira <bbf@charlietango.dk>
2025-01-04 18:55:48 +0100

    feat: add test for Question component

---
+++ src/components/Question/__test__/Question.test.tsx
@@ -0,0 +1,7 @@
+import Question from "../Question";
+import { render } from "@testing-library/react";
+import { questions } from "../../../api/mockData";
+
+test("should render <Question> without props", () => {
+  render(<Question {...questions[0]} questionId='question-1' />);
+});
