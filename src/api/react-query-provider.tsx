697d09a0a3b1950d06f2524f79ef8ded0673442b
Barbara Ferreira <bbf@charlietango.dk>
2025-01-06 12:24:27 +0100

    refactor: remove unnecessary things

--- src/api/react-query-provider.tsx
+++ src/api/react-query-provider.tsx
@@ -1,11 +1,9 @@
-"use client";
-
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export function ReactQueryProvider({ children }: React.PropsWithChildren) {
-  const [client] = React.useState(new QueryClient());
+export function ReactQueryProvider({ children }: PropsWithChildren) {
+  const client = new QueryClient();
 
   return (
     <QueryClientProvider client={client}>
