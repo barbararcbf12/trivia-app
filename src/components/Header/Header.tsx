f61c95394180cc9b86ef81340665bc92fbfbf0ee
Barbara Ferreira <bbf@charlietango.dk>
2025-01-06 21:17:08 +0100

    refactor: rename handler function for improving readability

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
 
   const { refetch, setAnswers, setIsFormSubmitted } = useQuestions();
 
-  const onHandleClick = () => {
+  const handleReloadClick = () => {
     setIsFormSubmitted(false);
     setAnswers([]);
     refetch();
@@ -16,7 +16,7 @@ function Header() {
     <header className="bg-grey-100 shadow-elevation-01 w-full text-primary-900 flex justify-between items-center py-3 px-6 sticky z-100 top-0">
       <h1 className="text-32">Trivia</h1>
       <nav>
-        <Button onClick={ onHandleClick }>Reload</Button>
+        <Button onClick={ handleReloadClick }>Reload</Button>
       </nav>
     </header>
   );
