169e17bb908bf62e39536bc77b988700ae1bd55d
Barbara Ferreira <bbf@charlietango.dk>
2025-01-08 22:15:54 +0100

    feat: create QuestionSkeleton and show it while data is not fetched

--- src/components/QuestionsList/QuestionsList.tsx
+++ src/components/QuestionsList/QuestionsList.tsx
@@ -1,10 +1,11 @@
 import React, { Dispatch, FormEvent, SetStateAction, useMemo } from "react";
 import Question from "../Question/Question";
 import Button from "../Button/Button";
-import { ApiDataProps } from "../../types/triviaApi";
 import { UseQueryResult } from "@tanstack/react-query";
-import { AnswerProps } from "../../types/answer";
+import type { ApiDataProps } from "../../types/triviaApi";
+import type { AnswerProps } from "../../types/answer";
 import { hasExceededRequestLimit } from "../../utils/hasExceededRequestLimit";
+import { Skeleton } from "@mui/material";
 
 type QuestionDataProps = {
   response?:  UseQueryResult<ApiDataProps, Error>,
@@ -39,9 +40,11 @@ function QuestionsList(props: QuestionDataProps) {
   const isFormFilled = answers.length === questions.length;
   const errorMessage = error?.message || "Something went wrong. Please try again.";
 
-  if (isLoading || isFetching || !enabled) return <span className="loader"></span>;
+  const loading = isLoading || isFetching || !enabled;
+
   if (exceedRequestsNr) return <span>Too many requests have occurred. Wait 5 seconds and try again.</span>;
   if (error) return <span>{`An error has occurred: ${errorMessage}`}</span>;
+  if (data?.results.length === 0) return <span>There are no questions for the selected options you made</span>;
 
   return (
     <>
@@ -49,7 +52,9 @@ function QuestionsList(props: QuestionDataProps) {
         onSubmit={ onHandleSubmit }
         className="w-full flex flex-col space-y-4"
       >
-        { questions?.map((question, index) =>
+        {loading ? [ ...Array(5) ].map((_, i) => (
+          <QuestionSkeleton key={`question-skeleton-${i}`}/>
+         )) : questions?.map((question, index) =>
           <Question
             key={ question.question }
             questionId={ `question-${ index + 1 }` }
@@ -58,6 +63,7 @@ function QuestionsList(props: QuestionDataProps) {
             setAnswers={setAnswers}
           />
         ) }
+        <QuestionSkeleton/>
         <Button disabled={ !isFormFilled }>Submit</Button>
       </form>
       <span className="text-red-600 pb-6 mb-6">{ isFormSubmitted ? `${ countCorrectAnswers } / 10 correct` : ''}</span>
@@ -66,3 +72,23 @@ function QuestionsList(props: QuestionDataProps) {
 }
 
 export default QuestionsList;
+
+const QuestionSkeleton = () => {
+  return (
+    <div className='flex flex-col space-y-2 rounded-mobile md:rounded-desktop bg-grey-100 p-6 shadow-elevation-01 w-full'>
+      <div className="flex flex-col space-y-4">
+        <Skeleton sx={ { height: 20, width: "80%" } } animation="wave" variant="rectangular"/>
+        <ul className="w-full flex flex-col gap-2.5">
+          { [ ...Array(5) ].map((_, i) => (
+            <li className="flex" key={ `option-skeleton-${ i }` }>
+              <span className="flex gap-2 items-center w-full">
+                <Skeleton sx={ {height: 20, width: 20} } animation="wave" variant="circular"/>
+                <Skeleton sx={ {height: 20, width: `${20 - i}%`} } animation="wave" variant="rectangular"/>
+              </span>
+            </li>
+          )) }
+        </ul>
+      </div>
+    </div>
+  );
+}
