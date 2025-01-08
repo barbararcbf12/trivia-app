24070abf990001c799ce95e40705b4154cd19099
Barbara Ferreira <bbf@charlietango.dk>
2025-01-08 20:41:58 +0100

    feat: add variant prop to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,16 @@
 import React, { ComponentPropsWithoutRef } from "react";
 
-type ButtonProps =  ComponentPropsWithoutRef<'button'>;
+type ButtonProps =  ComponentPropsWithoutRef<'button'> & {
+  variant?: 'primary' | 'secondary';
+}
 
 function Button(props: ButtonProps) {
-  const { children, disabled, ...rest } = props;
+  const { children, disabled, variant = 'primary', ...rest } = props;
+  const buttonColor = disabled ? 'bg-grey-200' : variant === 'secondary' ? 'bg-red-500' : 'bg-primary-500';
+
   return (
     <button
-      className={ `w-fit py-2 px-4 rounded-minimal text-white ${ disabled ? 'bg-grey-200' : 'bg-green-700'}` }
+      className={ `w-fit py-2 px-4 rounded-minimal text-white ${ buttonColor }` }
       disabled={disabled}
       {...rest}
     >
