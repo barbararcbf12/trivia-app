34160e892cdbec6d01fc8768689e441e54192234
Barbara Ferreira <bbf@charlietango.dk>
2025-01-08 15:06:17 +0100

    feat: create constant values for amount, difficulty and type options

---
+++ src/constants/query.ts
@@ -0,0 +1,16 @@
+export const AMOUNT_OPTIONS = [
+  { value: '10', name: '10' },
+  { value: '20', name: '20' },
+  { value: '50', name: '50' },
+];
+
+export const DIFFICULTY_OPTIONS = [
+  { value: 'easy', name: 'easy' },
+  { value: 'medium', name: 'medium' },
+  { value: 'hard', name: 'hard' },
+];
+
+export const TYPE_OPTIONS = [
+  { value: 'multiple', name: 'multiple' },
+  { value: 'boolean', name: 'boolean' },
+];
