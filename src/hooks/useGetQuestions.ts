249ea9690009b5b601f8f7b8209d290aeae3fee8
Barbara Ferreira <bbf@charlietango.dk>
2025-01-09 08:29:08 +0100

    refactor: useGetQuestions hook

--- src/hooks/useGetQuestions.ts
+++ src/hooks/useGetQuestions.ts
@@ -3,10 +3,8 @@ import { ApiDataProps } from "../types/triviaApi";
 import { getQuestions } from "../api/getQuestions";
 
 export function useGetQuestions(){
-  const { data: questionsData, isLoading: loadingQuestions, isFetching: fetchingQuestions, error: errorQuestions, refetch: refetchQuestions } = useQuery<ApiDataProps>({
+  return useQuery<ApiDataProps>({
     queryKey: ['triviaData'],
     queryFn: getQuestions,
   });
-
-  return { questionsData, loadingQuestions, fetchingQuestions, errorQuestions, refetchQuestions };
 }
