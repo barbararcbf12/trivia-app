3020fb5b7812ffcd577adb81a6426abdd9b8618f
Barbara Ferreira <bbf@charlietango.dk>
2025-01-09 09:53:43 +0100

    fix: error in getQuestions auxiliary function

--- src/api/getQuestions.ts
+++ src/api/getQuestions.ts
@@ -6,9 +6,11 @@ const QUESTIONS_API_URL = 'https://opentdb.com/api.php';
 export const getQuestions: (query: QueryProps) => Promise<ApiDataProps> = async (query: QueryProps): Promise<ApiDataProps> => {
   let searchParams = new URLSearchParams();
   if (query) {
-    Object.keys(query).forEach((queryKey) => {
-      // @ts-ignore
-      searchParams.set(queryKey, query[queryKey].toString());
+    (Object.keys(query) as (keyof QueryProps)[]).forEach((queryKey) => {
+      const value = query[queryKey];
+      if (value !== null && value !== undefined) {
+        searchParams.set(queryKey, value.toString());
+      }
     });
   }
   const response = await fetch(`${ QUESTIONS_API_URL }?${searchParams}`);
